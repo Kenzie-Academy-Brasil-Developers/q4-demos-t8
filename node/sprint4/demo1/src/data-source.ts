4c279c8e5c94d6cf6cb600dd25d84f0153979a08
pedrofhasler <pedro.hasler@gmail.com>
2022-05-30 13:41:56 -0300

    feat: sprint 4 demo 1

--- node/sprint4/demo1/src/data-source.ts
+++ node/sprint4/demo1/src/data-source.ts
@@ -1,14 +1,13 @@
-import "reflect-metadata";
-import { DataSource } from "typeorm";
-import path from "path";
-import * as dotenv from "dotenv";
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import path from 'path';
+import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const AppDataSource = new DataSource({
-  type: "postgres",
+  type: 'postgres',
   url: process.env.DATABASE_URL,
   logging: false,
-  ssl: { rejectUnauthorized: false },
-  entities: [path.join(__dirname, "./entities/**/*.{js,ts}")],
-  migrations: [path.join(__dirname, "./migrations/**/*.{js,ts}")],
+  entities: [path.join(__dirname, './entities/**/*.{js,ts}')],
+  migrations: [path.join(__dirname, './migrations/**/*.{js,ts}')],
 });
